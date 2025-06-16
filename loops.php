0e9d9d69b9cec99cc3f76919551a6b93d5b4db8e
Erick El nino <erick.tshimbombo@yahoo.com>
2025-06-16 20:42:37 +0200

    working on control structure

---
+++ loops.php
@@ -0,0 +1,20 @@
+<?php 
+   
+   $number = 10;
+
+   for ($i = 0; $i <= $number; $i++ ) {
+        echo "Number: {$i} <br>";
+   }
+
+   // while loops 
+
+
+   $index = 1;
+
+   while($index < 10) {
+
+    echo "while loop :  {$index} <br>";
+
+    $index++;
+
+   };
