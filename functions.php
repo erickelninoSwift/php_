54e75482295579644a737b2ac140e126bba7e079
Erick El nino <erick.tshimbombo@yahoo.com>
2025-06-16 15:59:49 +0200

    learning all basics

--- functions.php
+++ functions.php
@@ -1,3 +1,29 @@
 <?php
 
 // learn about functions in php
+
+function greet() {
+    echo "Welcome back everyone";
+}
+
+function addNumber ($num1,$num2) {
+    return $num1 + $num2;
+}
+
+// $total = (int) addNumber(10,25);
+// echo "<br>";
+// echo $total;
+
+
+// fimd even mumber;
+
+function isEventNumber ($number): bool {
+
+    echo $number . "<br>";
+    echo $number % 2 . "<br>";
+  return $number % 2 === 0 ? true : false;
+}
+
+isEventNumber(185);
+
+var_dump(isEventNumber(200));
