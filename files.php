154dce8f942218ed34493da3b16cad7155ae6198
Erick El nino <erick.tshimbombo@yahoo.com>
2025-06-21 02:12:06 +0200

    reading and appending to a file

--- files.php
+++ files.php
@@ -5,12 +5,29 @@ $currentDirectory = __DIR__;
 $filePath = __FILE__;
 
 
-$file = fopen($file_name,"r");
+$file = fopen($file_name,"w");
  
+// if($file) {
+//     $content = fread($file, filesize($file_name));
+//     echo nl2br($content);
+//     fclose($file);
+// } else {
+//      echo "Unable to open a file";
+// }
+
+// another way 
+
+$myContent = file_get_contents($file_name);
+echo nl2br($myContent);
+
+//write to a file 
+
 if($file) {
-    $content = fread($file, filesize($file_name));
-    echo nl2br($content);
-    fclose($file);
+     // write to a file 
+     if(file_put_contents($file_name, "You love me so much", FILE_APPEND | LOCK_EX ) !== false) {
+         echo "Data was appended successfully";
+     }
+   
 } else {
      echo "Unable to open a file";
 }
