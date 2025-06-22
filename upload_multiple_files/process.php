3326731f88bb1cc613186532bafbe98900e84a9b
Erick El nino <erick.tshimbombo@yahoo.com>
2025-06-22 20:49:07 +0200

    files uploads

--- upload_multiple_files/process.php
+++ upload_multiple_files/process.php
@@ -1,8 +1,27 @@
 <?php 
   // echo hello 
 
-  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if($_SERVER['REQUEST_METHOD'] === 'POST') {
     //
+    //
+    $upload_dir = "upload/";
+    
+    echo "<pre>";
+      var_dump($_FILES);
+    echo "</pre>";
 
-    echo "This is a post metho";
+    // if the directory doesnt not exist then create one
+    if(!is_dir($upload_dir) === true) {
+           mkdir($upload_dir,0777, true);
+    }
+    
+    // loop through all the files uploaded 
+    foreach($_FILES['files']['name'] as $key => $value) {
+       
+         echo "<pre>";
+        var_dump($value);
+         echo "</pre>";
+    }
+
+    
   }
