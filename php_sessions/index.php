c860aa2c9ef687dc97613c478e5488fa5958b03f
Erick El nino <erick.tshimbombo@yahoo.com>
2025-06-25 16:32:04 +0200

    working on sessions small login system

--- php_sessions/index.php
+++ php_sessions/index.php
@@ -2,7 +2,7 @@
 // session
 session_start();
 
-$_SESSION['username'] = 'Erick Tshimbombo Kazadi';
+$_SESSION['username'] = 'Erick Tshimbombo Kazadi' ?? "Guest";
 var_dump($_SESSION['username']);
 
 // session_destroy();
---
+++ php_sessions/login.php
@@ -0,0 +1,42 @@
+<?php
+
+  $error = "";
+
+   if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // let me echo out the post 
+    $user_admin = "admin";
+    $user_password = "#12345";
+
+    // check if the username is set 
+
+     $user_name = $_POST['username'];
+     $password = $_POST['password'];
+     
+     
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login </title>
+</head>
+
+<body>
+    <form action="login.php" method="POST">
+        <br>
+        <label for="username">Username</label>
+        <input id="username" type="text" name="username" placeholder="Username" />
+        <br>
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" placeholder="Password" />
+        <br>
+        <?php if(!empty($error)) { echo "<br>" . $error; } ?>
+        <br>
+        <input type="submit" value="Submit" />
+    </form>
+</body>
+
+</html>
