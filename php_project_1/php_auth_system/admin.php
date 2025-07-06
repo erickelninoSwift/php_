3fd7f59d3e0ee7bc8318f230e6900fb9b3142594
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-06 17:57:40 +0200

    update css

--- php_project_1/php_auth_system/admin.php
+++ php_project_1/php_auth_system/admin.php
@@ -28,6 +28,10 @@
         $result = mysqli_query($connection,$sql_query);
         // Fetch Associate Array
          if(check_query($result)) {
+            //
+            $_SESSION["message"] = "User was updated successfully !";
+            $_SESSION["msg_type"] = "success";
+            //
             redirect("admin.php");
          }
        }
@@ -41,6 +45,8 @@
              $sql_delete_user_query = "DELETE FROM users WHERE id='$user_id_to_delete'";
              $result_ = mysqli_query($connection,$sql_delete_user_query);
              if(check_query($result_)) {
+                  $_SESSION["message"] = "User was deleted successfully !";
+                  $_SESSION["msg_type"] = "success";
                 redirect("admin.php");
              }
         }
@@ -57,48 +63,84 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/admin.css">
     <title>Admin Page</title>
+
+    <style>
+    .container_box {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .container_box>.success {
+        height: 40px;
+        width: auto;
+        padding: 10px;
+        background-color: lightgreen;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 230px;
+        margin-bottom: 10px;
+    }
+
+    .container_box>.error {
+        height: 40px;
+        width: auto;
+        padding: 10px;
+        background-color: lightcoral;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 230px;
+        margin-bottom: 10px;
+    }
+    </style>
 </head>
 
 <?php include "partials/navigation.php" ?>
 <h1>Manage Users</h1>
 
 <div class="container">
-    <table class="user-table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Registration Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="container_box">
+        <div class="<?php echo $_SESSION['msg_type'];?>">
+            <?php echo $_SESSION['message']?>
+        </div>
+        <table class="user-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Registration Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
 
-            <?php while($fetch_users = mysqli_fetch_assoc($fetch_users_query)): ?>
-            <tr>
-                <td><?php echo $fetch_users['id']; ?></td>
-                <td><?php echo $fetch_users['username']; ?></td>
-                <td><?php echo $fetch_users['email']; ?></td>
-                <td><?php echo format_data_format($fetch_users['reg_date'])?></td>
-                <td>
-                    <form method="POST" style="display:inline-block;">
-                        <input type="hidden" name="user_id" value="<?php echo $fetch_users['id']; ?>">
-                        <input type="text" name="username" value="<?php echo $fetch_users['username']; ?>" required>
-                        <input type="email" name="email" value="<?php echo $fetch_users['email']; ?>" required>
-                        <button class="edit" type="submit" name="edit_user">Edit</button>
-                    </form>
-                    <form method="POST" style="display:inline-block;"
-                        onsubmit="return confirm('Are you sure you want to delete this user <?php echo $fetch_users['username'];?> ?');">
-                        <input type="hidden" name="user_id" value="<?php echo $fetch_users['id']; ?>">
-                        <button class="delete" type="submit" name="delete_user">Delete</button>
-                    </form>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-            <!-- Additional user rows can go here -->
-        </tbody>
-    </table>
+                <?php while($fetch_users = mysqli_fetch_assoc($fetch_users_query)): ?>
+                <tr>
+                    <td><?php echo $fetch_users['id']; ?></td>
+                    <td><?php echo $fetch_users['username']; ?></td>
+                    <td><?php echo $fetch_users['email']; ?></td>
+                    <td><?php echo format_data_format($fetch_users['reg_date'])?></td>
+                    <td>
+                        <form method="POST" style="display:inline-block;">
+                            <input type="hidden" name="user_id" value="<?php echo $fetch_users['id']; ?>">
+                            <input type="text" name="username" value="<?php echo $fetch_users['username']; ?>" required>
+                            <input type="email" name="email" value="<?php echo $fetch_users['email']; ?>" required>
+                            <button class="edit" type="submit" name="edit_user">Edit</button>
+                        </form>
+                        <form method="POST" style="display:inline-block;"
+                            onsubmit="return confirm('Are you sure you want to delete this user <?php echo $fetch_users['username'];?> ?');">
+                            <input type="hidden" name="user_id" value="<?php echo $fetch_users['id']; ?>">
+                            <button class="delete" type="submit" name="delete_user">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+                <!-- Additional user rows can go here -->
+            </tbody>
+        </table>
+    </div>
 </div>
 
 </html>
