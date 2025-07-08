09a2f194537167f65a465ac38f8a9eb03a8f208a
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-08 14:13:37 +0200

    managed to avoid sql injection by fetching all users or sleected users  from the db

--- sql_injection__/sql_injection/index.php
+++ sql_injection__/sql_injection/index.php
@@ -6,5 +6,38 @@
     }else {
         echo "connected";
     }
+    $username = "john_doe";
+    // we need to avoid the sql injection by adding prepare statement
     
+    // $sql_query = "SELECT * FROM users WHERE user_name='john_doe'";
+    // $result = mysqli_query($connect,$sql_query);
+    // different query for prepare statement
+      $sql_query = "SELECT * FROm users WHERE user_name= ?";
+      // prepare query 
+      $stmt = mysqli_prepare($connect,$sql_query);
+      // bidn data to the query
+      mysqli_stmt_bind_param($stmt,"s", $username);
+
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+     
 ?>;
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Display array data</title>
+</head>
+
+<body>
+    <ul>
+        <?php while($asso_array = mysqli_fetch_assoc($result)): ?>
+        <li><?php echo $asso_array['user_name'];?></li>
+        <?php endwhile; ?>
+    </ul>
+</body>
+
+</html>
