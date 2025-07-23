34979e9bfe17f561b426282bfd218e5c8aadb9c1
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-23 21:42:30 +0200

    class static

--- OOP_Fundamentals/class_delcaration.php
+++ OOP_Fundamentals/class_delcaration.php
@@ -1,68 +1,68 @@
 <?php 
-class User {
+// class User {
     
 
-    // public
-    public $username;
+//     // public
+//     public $username;
 
-    public $email;
+//     public $email;
 
-    //private
+//     //private
      
 
-    // protected
+//     // protected
 
 
-    public function __construct($name, $email) {
+//     public function __construct($name, $email) {
        
-        $this->username = $name;
-        $this->email = $email;
-    }
+//         $this->username = $name;
+//         $this->email = $email;
+//     }
 
-    public function setUserName () {
-        echo $this->username;
-    }
+//     public function setUserName () {
+//         echo $this->username;
+//     }
 
 
-    public function displayUserinfo() {
-        echo " My name is : " . $this->username . " and my email is : " . $this->email . "<br>";
-    }
+//     public function displayUserinfo() {
+//         echo " My name is : " . $this->username . " and my email is : " . $this->email . "<br>";
+//     }
     
-}
+// }
 
 
-class AdminUser extends User {
+// class AdminUser extends User {
 
-   public $role;
+//    public $role;
 
-   public function __construct($name, $email, $role) {
+//    public function __construct($name, $email, $role) {
 
-    //
-    $this->role = $role;
-    parent::__construct($name,$email);
+//     //
+//     $this->role = $role;
+//     parent::__construct($name,$email);
 
-   }
+//    }
 
-   public function handleNewData() {
-      echo " my new role is : {$this->role}";
-   }
+//    public function handleNewData() {
+//       echo " my new role is : {$this->role}";
+//    }
 
-}
-
-
-$user = new User("erick", "erick@yahoo.com");
-
-$admin = new AdminUser("Jackenson Verdul", "Jackpot@yahoo.com", "Admin");
+// }
 
 
+// $user = new User("erick", "erick@yahoo.com");
 
-$user->displayUserinfo();
+// $admin = new AdminUser("Jackenson Verdul", "Jackpot@yahoo.com", "Admin");
 
-$admin->displayUserinfo();
 
-$admin->handleNewData();
 
-echo "my name is : " . $admin->username;
+// $user->displayUserinfo();
+
+// $admin->displayUserinfo();
+
+// $admin->handleNewData();
+
+// echo "my name is : " . $admin->username;
 
 
 class UserDetails  {
@@ -89,6 +89,13 @@ class UserDetails  {
 
 class Student extends UserDetails {
    
+    public function display_password_student() {
+        return $this->password;
+    }
+
+    public function display_email() {
+        $this->display_email();
+    }
     
 }
 
@@ -102,4 +109,8 @@ echo 'Play with' .  $user_details->username . "<br>";
 echo "From user details : my email address is " . $student->username . "<br>";
 // password
 
-echo 'try to display protected variable' . $student->display_password();
+echo 'try to display protected variable' . $student->display_password() . "<br>";
+echo "My Password : " .$student->display_password_student() . "<br>";
+
+//
+//static 
