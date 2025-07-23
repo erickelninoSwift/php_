c7ee0d122b3c8d7cd610f0f19a567966ba46eeb8
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-23 22:01:35 +0200

    update now wroking on getters and setters

--- OOP_Fundamentals/class_static.php
+++ OOP_Fundamentals/class_static.php
@@ -1,3 +1,22 @@
 <?php 
 
-echo "Class Static";
+
+class User {
+
+    public $user_email = "Erickelnino";
+    public static $user_count = 0;
+    protected static $email = "erick@yahoo.com";
+
+
+    public function __construct() {
+
+    }
+
+    public function email_user() {
+        return self::$email;
+    }
+}
+
+$current_user = new User();
+echo "User count: " . User::$user_count . "<br>";
+echo "User email : " . $current_user->email_user();
