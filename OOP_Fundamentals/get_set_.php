28c429c956505ff97278fe99514ee4f631a6ff1e
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-24 20:29:34 +0200

    update

--- OOP_Fundamentals/get_set_.php
+++ OOP_Fundamentals/get_set_.php
@@ -1,3 +1,36 @@
 <?php 
 
-echo "getters and setters";
+class User {
+
+
+    private $username;
+    private $email;
+
+    public function __construct($username,$email){
+          
+        $this->username = $username;
+        $this->email = $email;
+    }
+  
+    // getters
+    public function getEmail() {
+        return $this->email;
+    }
+
+
+    // setters
+
+    public function setEmail($email) {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $this->email = $email;
+        }else {
+            echo "data invalid";
+        }
+    }
+    
+}
+
+
+$user = new User("jackenson verdul", "jackpot@yahoo.com");
+
+echo 'Your email is. : ' . $user->getEmail();
