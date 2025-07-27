8da9f3894ebcca1c5db8d0efc85762ad19bab11e
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-27 18:33:02 +0200

    create stmt for creating task oop way

--- OOP_Fundamentals/todo_app_oop/classes/Task.php
+++ OOP_Fundamentals/todo_app_oop/classes/Task.php
@@ -2,12 +2,36 @@
 
 class Task {
 
+    private $connection;
+    private $table = 'todo_app';
     public $task_name;
-    public $isCompleted;
+    public $is_completed;
 
-    public function __construct($task_name, $isCompleted) {
+    public function __construct($db_connection, $task_name, $isCompleted) {
+        $this->connection = $db_connection;
         $this->task_name = $task_name;
-        $this->isCompleted = $isCompleted;
+        $this->is_completed = $isCompleted;
+    }
+
+    public function create_task (){
+        // write the query to add data to db 
+        $query = "INSERT INTO ". $this->table ."(task) VALUES (?)";
+        $stmt = mysqli_prepare($this->connection,$query);
+
+        if(!$stmt) {
+            die('Prepare failed: ' . mysqli_error($this->connection));
+        }
+        //
+        $stmt->bind_param("s" ,$this->task_name);
+        $stmt->execute();
+         
+        if($stmt->affected_rows > 0) {
+            echo "Task Added";
+        }else {
+            echo 'Failed to create a Task';
+        }
+        //
+        $stmt->close();
     }
 
 }
